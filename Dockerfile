4b0143d629a6969daddfd12841d61d52db363db6
AntonShmyg <125025944+AntonShmyg@users.noreply.github.com>
2024-02-26 22:29:14 +0300

    1

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
 FROM python:3.6
 RUN apt-get update
+
+RUN mkdir /University
 WORKDIR /University
-COPY . /app
+COPY . /University
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 5000
--- query/pg_up_1.sql
+++ query/pg_up_1.sql
@@ -1,13 +1,24 @@
 
+
+CREATE TABLE lessons(
+    id SERIAL PRIMARY KEY,
+    object varchar(100) NOT NULL,
+    date TIMESTAMP NOT NULL,
+    class INT NOT NULL,
+    eqiup varchar(60)
+);
+
 CREATE TABLE specialities(
     id SERIAL PRIMARY KEY,
     naming VARCHAR(60) NOT NULL,
-    code VARCHAR(10) NOT NULL
+    code VARCHAR(10) NOT NULL,
+    department_id INT NOT NULL
 );
 
 CREATE TABLE groups(
     id SERIAL PRIMARY KEY,
     depart_id INT NOT NULL,
+    group_code VARCHAR(20) NOT NULL,
     spec_id INT NOT NULL,
     FOREIGN KEY (spec_id) REFERENCES specialities(id)
 );
@@ -16,44 +27,22 @@ CREATE TABLE students(
     id SERIAL PRIMARY KEY,
     full_name VARCHAR(60) NOT NULL,
     group_id INT NOT NULL,
-    cours_id INT NOT NULL,
-    FOREIGN KEY (group_id) REFERENCES groups(id),
-    FOREIGN KEY (course_id) REFERENCES courses(id)
-);
-
-CREATE TABLE objects(
-    id SERIAL PRIMARY KEY,
-    naming varchar(30) NOT NULL,
-    lection_id INT NOT NULL,
-    FOREIGN KEY (lection_id) REFERENCES lections(id)
-);
-
-CREATE TABLE courses(
-    id SERIAL PRIMARY KEY,
-    naming VARCHAR(60) NOT NULL,
     spec_id INT NOT NULL,
-    obj_id INT NOT NULL,
-    FOREIGN KEY (spec_id) REFERENCES specialities(id),
-    FOREIGN KEY (obj_id) REFERENCES objects(id)
+    FOREIGN KEY (group_id) REFERENCES groups(id),
+    FOREIGN KEY (spec_id) REFERENCES specialities(id)
 );
 
-CREATE TABLE schedules(
-    id SERIAL PRIMARY KEY,
-    obj_id INT NOT NULL,
-    date TIMESTAMP NOT NULL,,
-    FOREIGN KEY (obj_id) REFERENCES objects(id)
+CREATE TABLE schedule(
+    lesson_id INT NOT NULL,
+    group_id INT NOT NULL,
+    FOREIGN KEY (lesson_id) REFERENCES lessons(id),
+    FOREIGN KEY (group_id) REFERENCES groups(id)
 );
 
-CREATE TABLE attendances(
+CREATE TABLE journal(
     id SERIAL PRIMARY KEY,
     stud_id INT NOT NULL,
-    sched_id INT NOT NULL,
+    lesson_id INT NOT NULL,
     FOREIGN KEY (stud_id) REFERENCES students(id),
-    FOREIGN KEY (sched_id) REFERENCES schedules(id)
-);
-
-CREATE TABLE lections(
-    id SERIAL PRIMARY KEY,
-    material_id INT NOT NULL,
-    eqiup varchar(60)
+    FOREIGN KEY (lesson_id) REFERENCES lessons(id)
 );
